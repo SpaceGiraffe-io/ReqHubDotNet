ec99841d4696fa944c3306a014f089af53fc15be
atemple <templeandrew@outlook.com>
2020-02-06 10:12:03 -0500

    Tweak how the httpClient gets created

--- ReqHubNuGet/Services/ApiClient/ApiClient.cs
+++ ReqHubNuGet/Services/ApiClient/ApiClient.cs
@@ -11,20 +11,16 @@ namespace ReqHub
 {
     public class ApiClient : IApiClient
     {
-        private readonly IHttpClientFactory httpClientFactory;
-        private readonly string name;
+        private readonly HttpClient httpClient;
 
         public ApiClient(IHttpClientFactory httpClientFactory, string name)
         {
-            this.httpClientFactory = httpClientFactory;
-            this.name = name;
+            this.httpClient = httpClientFactory.CreateClient(name);
         }
 
         public async Task<TResult> GetAsync<TResult>(string path, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var httpClient = this.httpClientFactory.CreateClient(this.name);
-
-            var response = await httpClient.GetAsync(path, cancellationToken);
+            var response = await this.httpClient.GetAsync(path, cancellationToken);
             var result = await response.Content.ReadAsAsync<TResult>(cancellationToken);
 
             return result;
@@ -32,9 +28,7 @@ namespace ReqHub
 
         public async Task<TResult> PostAsync<TResult>(string path, HttpContent content, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var httpClient = this.httpClientFactory.CreateClient(this.name);
-
-            var response = await httpClient.PostAsync(path, content, cancellationToken);
+            var response = await this.httpClient.PostAsync(path, content, cancellationToken);
             var result = await response.Content.ReadAsAsync<TResult>(cancellationToken);
 
             return result;
@@ -42,9 +36,7 @@ namespace ReqHub
 
         public async Task<TResult> PutAsync<TResult>(string path, HttpContent content, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var httpClient = this.httpClientFactory.CreateClient(this.name);
-
-            var response = await httpClient.PutAsync(path, content, cancellationToken);
+            var response = await this.httpClient.PutAsync(path, content, cancellationToken);
             var result = await response.Content.ReadAsAsync<TResult>(cancellationToken);
 
             return result;
@@ -52,9 +44,7 @@ namespace ReqHub
 
         public async Task<TResult> PatchAsync<TResult>(string path, HttpContent content, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var httpClient = this.httpClientFactory.CreateClient(this.name);
-
-            var response = await httpClient.PatchAsync(path, content, cancellationToken);
+            var response = await this.httpClient.PatchAsync(path, content, cancellationToken);
             var result = await response.Content.ReadAsAsync<TResult>(cancellationToken);
 
             return result;
@@ -62,9 +52,7 @@ namespace ReqHub
 
         public async Task<TResult> DeleteAsync<TResult>(string path, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var httpClient = this.httpClientFactory.CreateClient(this.name);
-
-            var response = await httpClient.DeleteAsync(path, cancellationToken);
+            var response = await this.httpClient.DeleteAsync(path, cancellationToken);
             var result = await response.Content.ReadAsAsync<TResult>(cancellationToken);
 
             return result;
@@ -72,8 +60,6 @@ namespace ReqHub
 
         public async Task<HttpResponseMessage> SendAsync(string path, HttpMethod method, HttpContent content = null, IDictionary<string, string> headers = null, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var httpClient = this.httpClientFactory.CreateClient(this.name);
-
             var request = new HttpRequestMessage
             {
                 RequestUri = new Uri(path, UriKind.Relative),
@@ -89,15 +75,13 @@ namespace ReqHub
                 }
             }
 
-            var response = await httpClient.SendAsync(request, cancellationToken);
+            var response = await this.httpClient.SendAsync(request, cancellationToken);
             return response;
         }
 
         public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var httpClient = this.httpClientFactory.CreateClient(this.name);
-
-            var response = await httpClient.SendAsync(request, cancellationToken);
+            var response = await this.httpClient.SendAsync(request, cancellationToken);
             return response;
         }
     }
