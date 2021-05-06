efce7267509f6edc409941cf759d8082701c1db0
atemple <templeandrew@outlook.com>
2021-05-06 12:12:15 -0400

    Fix user home directory in generate-coverage-report.bat

--- ReqHubDotNet.Tests/generate-coverage-report.bat
+++ ReqHubDotNet.Tests/generate-coverage-report.bat
@@ -1,4 +1,4 @@
 
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Include=\"[ReqHubDotNet]ReqHub.*\"
 
-dotnet "C:\Users\templ\.nuget\packages\reportgenerator\4.0.4\tools\netcoreapp2.0\ReportGenerator.dll" "-reports:.\coverage.opencover.xml" "-targetdir:.\coverage"
+dotnet "%userprofile%\.nuget\packages\reportgenerator\4.0.4\tools\netcoreapp2.0\ReportGenerator.dll" "-reports:.\coverage.opencover.xml" "-targetdir:.\coverage"
